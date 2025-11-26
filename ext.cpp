58624e2d2abad36d447714ef2a71463fa3d23625
Pawel Tomasz Pieta <papi@comp-gpu04.compute.dtu.dk>
2025-11-26 15:09:25 +0100

    First version, 11x faster than baseline

--- ext.cpp
+++ ext.cpp
@@ -1,10 +1,13 @@
 #include <torch/extension.h>
 #include "ssim3d.h"
 #include "ssim2d.h"
+#include "ssim3d_by_2d.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("fusedssim3d", &fusedssim3d);
   m.def("fusedssim_backward3d", &fusedssim_backward3d);
   m.def("fusedssim2d", &fusedssim2d);
   m.def("fusedssim_backward2d", &fusedssim_backward2d);
+  m.def("fusedssim3d_by_2d", &fusedssim3d_by_2d);
+  m.def("fusedssim_backward3d_by_2d", &fusedssim_backward3d_by_2d);
 }
--- fused_ssim3d/__init__.py
+++ fused_ssim3d/__init__.py
@@ -2,7 +2,7 @@ from typing import NamedTuple
 import torch.nn as nn
 import torch
 
-from fused_ssim3d_cuda import fusedssim3d, fusedssim_backward3d, fusedssim2d, fusedssim_backward2d
+from fused_ssim3d_cuda import fusedssim3d, fusedssim_backward3d, fusedssim2d, fusedssim_backward2d, fusedssim3d_by_2d, fusedssim_backward3d_by_2d
 
 
 allowed_padding = ["same", "valid"]
@@ -10,11 +10,14 @@ allowed_padding = ["same", "valid"]
     
 class FusedSSIMMap(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, C1, C2, img1, img2, padding="same", train=True, spatial_dims=2):
+    def forward(ctx, C1, C2, img1, img2, padding="same", train=True, spatial_dims=2, mode="default"):
         if spatial_dims == 2:
             ssim_map, dm_dmu1, dm_dsigma1_sq, dm_dsigma12 = fusedssim2d(C1, C2, img1, img2, train)
         elif spatial_dims == 3:
-            ssim_map, dm_dmu1, dm_dsigma1_sq, dm_dsigma12 = fusedssim3d(C1, C2, img1, img2, train)
+            if mode == "default":
+                ssim_map, dm_dmu1, dm_dsigma1_sq, dm_dsigma12 = fusedssim3d(C1, C2, img1, img2, train)
+            elif mode == "by_2d":
+                ssim_map, dm_dmu1, dm_dsigma1_sq, dm_dsigma12 = fusedssim3d_by_2d(C1, C2, img1, img2, train)
         
         if padding == "valid":
             if spatial_dims == 2:
@@ -27,6 +30,7 @@ class FusedSSIMMap(torch.autograd.Function):
         ctx.C2 = C2
         ctx.padding = padding
         ctx.spatial_dims = spatial_dims
+        ctx.mode = mode
 
         return ssim_map
 
@@ -44,8 +48,11 @@ class FusedSSIMMap(torch.autograd.Function):
         if ctx.spatial_dims == 2:
             grad = fusedssim_backward2d(C1, C2, img1, img2, dL_dmap, dm_dmu1, dm_dsigma1_sq, dm_dsigma12)
         elif ctx.spatial_dims == 3:
-            grad = fusedssim_backward3d(C1, C2, img1, img2, dL_dmap, dm_dmu1, dm_dsigma1_sq, dm_dsigma12)
-        return None, None, grad, None, None, None, None
+            if ctx.mode == "default":
+                grad = fusedssim_backward3d(C1, C2, img1, img2, dL_dmap, dm_dmu1, dm_dsigma1_sq, dm_dsigma12)
+            elif ctx.mode == "by_2d":
+                grad = fusedssim_backward3d_by_2d(C1, C2, img1, img2, dL_dmap, dm_dmu1, dm_dsigma1_sq, dm_dsigma12)
+        return None, None, grad, None, None, None, None, None
 
 def fused_ssim3d(img1, img2, padding="same", train=True):
     C1 = 0.01 ** 2
@@ -54,7 +61,7 @@ def fused_ssim3d(img1, img2, padding="same", train=True):
     assert padding in allowed_padding
 
     img1 = img1.contiguous()
-    map = FusedSSIMMap.apply(C1, C2, img1, img2, padding, train, 3)
+    map = FusedSSIMMap.apply(C1, C2, img1, img2, padding, train, 3, "default")
     return map.mean()
 
 def fused_ssim2d(img1, img2, padding="same", train=True):
@@ -64,5 +71,15 @@ def fused_ssim2d(img1, img2, padding="same", train=True):
     assert padding in allowed_padding
 
     img1 = img1.contiguous()
-    map = FusedSSIMMap.apply(C1, C2, img1, img2, padding, train, 2)
+    map = FusedSSIMMap.apply(C1, C2, img1, img2, padding, train, 2, "default")
+    return map.mean()
+
+def fused_ssim3d_by_2d(img1, img2, padding="same", train=True): 
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    assert padding in allowed_padding
+
+    img1 = img1.contiguous()
+    map = FusedSSIMMap.apply(C1, C2, img1, img2, padding, train, 3, "by_2d")
     return map.mean()
